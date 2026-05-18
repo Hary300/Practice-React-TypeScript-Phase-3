dbd81f15b02564bc83579db4c90c8753dc55d252
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-05-18 23:04:56 +0700

    topic 10: React Router (multi page)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import ProfileForm from './components/ProfileForm';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <ProfileForm />
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </>
   );
 }
